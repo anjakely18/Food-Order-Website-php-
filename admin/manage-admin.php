23944230e00e335b3431d022143707995fd04a1f
anjakely18 <anjaniaina03@gmail.com>
2024-06-11 21:43:58 +0200

    admin panel

---
+++ admin/manage-admin.php
@@ -0,0 +1,12 @@
+<?php include('partials/menu.php')?>
+
+        <!-- Main Section Starts -->
+        <div class="main-content">
+            <div class="wrapper">
+                 <h1>Manage Admin</h1>
+
+
+            </div>
+         </div>
+        <!-- Main Section Ends --> 
+<?php include('partials/footer.php')?>
---
+++ admin/partials/footer.php
@@ -0,0 +1,7 @@
+        <!-- Footer Section Starts -->
+        <div class="footer">
+            <div class="wrapper">
+                 <p class="text-center">2024 All Right reserved, Some Restaurant. Developped By - <a href="#"> Anja Rasamoelina </a></p>
+            </div>
+         </div>
+        <!-- Footer Section Ends --> 
